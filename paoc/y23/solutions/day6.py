987f0f55ac98709ce18ef54f0c7e26fc4e53834d
Josef Hamelink <91252200+Josef-Hlink@users.noreply.github.com>
2023-12-06 15:29:36 +0100

    Miniscule performance improvement
    2 less operations:
    - since x1 (floored part) is always greater than x2 (ceiled part) we can omit taking the absolute
    - since we don't take the absolute, we can also write `abs(x-1) + 2` as `x + 1`
    
    TLDR; shorter code and shorter runtime :+1:

--- paoc/y23/solutions/day6.py
+++ paoc/y23/solutions/day6.py
@@ -10,7 +10,7 @@ import math
 
 def ways_to_beat(t: int, d: int) -> int:
     """ ax^2 + bx + c = 0 with a=-1, b=t, c=-d [args: `t` time to beat, `d` distance] """
-    return abs(math.floor((-t - (sqD := math.sqrt(t**2 - 4*d))) / (-2) - 1e-9) - math.ceil((-t + sqD) / (-2) + 1e-9) - 1) + 2
+    return math.floor((-t - (sqD := math.sqrt(t**2 - 4*d))) / (-2) - 1e-9) - math.ceil((-t + sqD) / (-2) + 1e-9) + 1
 
 def p1() -> any:
     return math.prod([ways_to_beat(t, d) for t, d in zip(*map(lambda line: [int(x) for x in re.findall(r'\d+', line)], get_input(6)))])
