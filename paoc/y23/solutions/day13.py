c6a9b1ae2bf3077efd10754218e7001d6ddaf463
Josef Hamelink <91252200+Josef-Hlink@users.noreply.github.com>
2023-12-13 22:34:29 +0100

    Smarter way to solve p2

--- paoc/y23/solutions/day13.py
+++ paoc/y23/solutions/day13.py
@@ -11,12 +11,15 @@ def parse_input(lines: list[str]) -> list[np.ndarray]:
     """ TODO: add param to `get_input` to get raw contents or make new function for this (to preserve type correctness). """
     return [np.array([list(map('.#'.index, row)) for row in grid.split('\n')], dtype=bool) for grid in '\n'.join(lines).split('\n\n')]
 
-def find_mirror(grid: np.ndarray, skip_split: int = 0) -> int:
+def find_mirror(grid: np.ndarray, smudged: bool = False) -> int:
+    orig_split = find_mirror(grid, smudged=False) if smudged else 0
     for split in range(1, grid.shape[0]):
-        if split == skip_split:
+        if split == orig_split:
             continue
+        violations = 0
         for back, forward in zip(reversed(grid[:split]), grid[split:]):
-            if any(back != forward):
+            violations += sum(back != forward)
+            if violations > smudged:
                 break
         else:
             return split
@@ -27,30 +30,15 @@ def p1() -> int:
     res = 0
     for grid in grids:
         res += 100 * find_mirror(grid)
-        res += find_mirror(np.transpose(grid))
+        res += find_mirror(grid.T)
     return res
 
 def p2() -> int:
     grids = parse_input(get_input(13))
     res = 0
     for grid in grids:
-        orig_hor = find_mirror(grid)
-        orig_ver = find_mirror(np.transpose(grid))
-        smudge_found = False
-        for i in range(grid.shape[0]):
-            for j in range(grid.shape[1]):
-                grid_ = grid.copy()
-                grid_[i,j] = not grid_[i,j]
-                if (hor := find_mirror(grid_, skip_split=orig_hor)):
-                    smudge_found = True; break
-                if (ver := find_mirror(np.transpose(grid_), skip_split=orig_ver)):
-                    smudge_found = True; break
-            if smudge_found:
-                res += 100 * hor
-                res += ver
-                break
-        else:
-            assert False
+        res += 100 * find_mirror(grid, smudged=True)
+        res += find_mirror(grid.T, smudged=True)
     return res
 
 
