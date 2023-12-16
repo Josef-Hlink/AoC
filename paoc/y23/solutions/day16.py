fc007a9ce5beb981bb254da8f4daf4f52e65783f
Josef Hamelink <91252200+Josef-Hlink@users.noreply.github.com>
2023-12-16 17:32:10 +0100

    Add visualization

--- paoc/y23/solutions/day16.py
+++ paoc/y23/solutions/day16.py
@@ -4,6 +4,9 @@
 """ AoC 23 Day 16 - The Floor Will Be Lava """
 
 from paoc.helper import get_input, print_summary
+import os
+import sys
+import time
 
 
 N = (-1, 0)
@@ -20,9 +23,34 @@ class Photon:
         next_pos = (self.pos[0] + self.dir[0], self.pos[1] + self.dir[1])
         self.pos = next_pos
 
-def find_energized(contraption: list[str], origin: Photon) -> set[tuple[int, int]]:
+class Contraption:
+    def __init__(self, lines: list[str], visual: bool = False):
+        self.raw = lines
+        self.nr = len(lines)
+        self.nc = len(lines[0])
+        self.energized: set[tuple[int, int]] = set()
+        self.visual = visual
+        if visual:
+            self.processed = [['#'] * (self.nc + 2)] + [list(f'#{line}#') for line in self.raw] + [['#'] * (self.nc + 2)]
+    
+    def __getitem__(self, key: int) -> str:
+        return self.raw[key]
+    
+    def energize(self, pos: tuple[int, int]) -> None:
+        self.energized.add(pos)
+        if self.visual:
+            r, c = pos
+            self.processed[r+1][c+1] = f"\033[30;43m{self.raw[r][c]}\033[0m"
+    
+    def copy(self) -> "Contraption":
+        return Contraption(self.raw.copy())
+    
+    def render(self) -> None:
+        os.system('clear')
+        print('\n'.join(''.join(line) for line in self.processed))
+        time.sleep(1)
 
-    nr, nc = len(contraption), len(contraption[0])
+def energize(contraption: Contraption, origin: Photon) -> Contraption:
 
     mirrors = {
         '/':  {N: E,  S: W,  W: S,  E: N},
@@ -36,11 +64,10 @@ def find_energized(contraption: list[str], origin: Photon) -> set[tuple[int, int
     photons = [origin]
     sources = {(origin.pos, origin.dir)}
 
-    energized: set[tuple[int, int]] = set()
     while photons:
         for p in photons:
-            while -1 < p.pos[0] < nr and -1 < p.pos[1] < nc:
-                energized.add(p.pos)
+            while -1 < p.pos[0] < contraption.nr and -1 < p.pos[1] < contraption.nc:
+                contraption.energize(p.pos)
                 tile_type = contraption[p.pos[0]][p.pos[1]]
                 if tile_type == '.':
                     # this happens often, so we save time by skipping slightly more expensive checks
@@ -59,24 +86,28 @@ def find_energized(contraption: list[str], origin: Photon) -> set[tuple[int, int
                     break
                 p.move()
             photons.remove(p)
-    return energized
+        if contraption.visual:
+            contraption.render()
+    return contraption
 
 
 def p1() -> int:
-    return len(find_energized(get_input(16), Photon((0, 0), E)))
+    return len(energize(Contraption(get_input(16)), Photon((0, 0), E)).energized)
 
 def p2() -> int:
-    contraption = get_input(16)
-    nr, nc = len(contraption), len(contraption[0])
+    contraption = Contraption(get_input(16))
     max_energized = 0
-    for r in range(nr):
-        max_energized = max(max_energized, len(find_energized(contraption, Photon((r, 0), E))))
-        max_energized = max(max_energized, len(find_energized(contraption, Photon((r, nc-1), W))))
-    for c in range(nr):
-        max_energized = max(max_energized, len(find_energized(contraption, Photon((0, c), S))))
-        max_energized = max(max_energized, len(find_energized(contraption, Photon((nr-1, c), N))))
+    for r in range(contraption.nr):
+        max_energized = max(max_energized, len(energize(contraption.copy(), Photon((r, 0), E)).energized))
+        max_energized = max(max_energized, len(energize(contraption.copy(), Photon((r, contraption.nc-1), W)).energized))
+    for c in range(contraption.nr):
+        max_energized = max(max_energized, len(energize(contraption.copy(), Photon((0, c), S)).energized))
+        max_energized = max(max_energized, len(energize(contraption.copy(), Photon((contraption.nr-1, c), N)).energized))
     return max_energized
 
 
 if __name__ == '__main__':
-    print_summary(__file__, p1, p2, n=10)
+    if len(sys.argv) > 1 and sys.argv[1] == 'visualize':
+        _ = energize(Contraption(get_input(16), visual=True), Photon((0, 0), E))
+    else:
+        print_summary(__file__, p1, p2, n=10)
