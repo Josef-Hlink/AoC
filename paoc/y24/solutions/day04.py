433f285605b64864dc900d7f3ae4c81b0b1a6b97
Josef Hamelink <91252200+Josef-Hlink@users.noreply.github.com>
2024-12-04 12:57:54 +0100

    Clean up day 4

--- paoc/y24/solutions/day04.py
+++ paoc/y24/solutions/day04.py
@@ -4,44 +4,42 @@
 """ AoC 24 Day 4 - Ceres Search """
 
 from paoc.helper import get_input, print_summary
+from itertools import product
 
 
 def p1() -> any:
+    """ Find XMAS in any direction, overlaps allowed """
     grid = get_input(4)
     pattern = 'XMAS'
-    dirs = [(-1, -1), (-1, +0), (-1, +1),
-            (+0, -1),           (+0, +1),
-            (+1, -1), (+1, +0), (+1, +1)]
-    count = 0
-    I, J = range(len(grid)), range(len(grid[0]))
+    dirs = list(product((-1, 0, +1), repeat=2))  # all 9 directions
+    dirs.remove((0, 0))  # remove "stationary" direction
+    count, I, J = 0, range(len(grid)), range(len(grid[0]))
 
-    for i in I:
-        for j in J:
-            if grid[i][j] != 'X':
-                continue
-            for i_, j_ in dirs:
-                for n in range(1, 4):
-                    ni, nj = i+i_*n, j+j_*n
-                    if ni not in I or nj not in J or grid[ni][nj] != pattern[n]:
-                        break
-                else:
-                    count += 1
+    for i, j in product(I, J):
+        if grid[i][j] != 'X':
+            continue
+        for i_, j_ in dirs:  # check every direction
+            for n in range(1, 4):
+                ni, nj = i+i_*n, j+j_*n
+                if ni not in I or nj not in J or grid[ni][nj] != pattern[n]:
+                    break
+            else:  # loop not broken out of; full pattern found
+                count += 1
 
     return count
 
 def p2() -> any:
+    """ Find two MAS patterns that cross (X) each other at the A """
     grid = get_input(4)
-    dirs = [(-1, -1), (-1, +1), (+1, -1), (+1, +1)]
-    count = 0
-    I, J = range(1, len(grid)-1), range(1, len(grid[0])-1)
-
-    for i in I:
-        for j in J:
-            if grid[i][j] != 'A':
-                continue
-            neighbors = [grid[i+i_][j+j_] for i_, j_ in dirs]
-            if neighbors.count('M') == 2 and neighbors.count('S') == 2 and ''.join(neighbors) not in ['MSSM', 'SMMS']:
-                count += 1
+    dirs = list(product((-1, +1), repeat=2))  # diagonal (X) directions
+    count, I, J = 0, range(1, len(grid)-1), range(1, len(grid[0])-1)
+    
+    for i, j in product(I, J):
+        if grid[i][j] != 'A':
+            continue
+        X = [grid[i+i_][j+j_] for i_, j_ in dirs]  # X-neighbors
+        if X.count('M') == X.count('S') == 2 and X[1] != X[2]:  # prevent MAMxSAS
+            count += 1
     
     return count
 
