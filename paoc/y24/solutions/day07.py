21000471665e36e2731465efbf05721816e42121
Josef Hamelink <91252200+Josef-Hlink@users.noreply.github.com>
2024-12-07 12:50:32 +0100

    Solve day 7
    very easy one today, but a bit slow sadly

---
+++ paoc/y24/solutions/day07.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" AoC 24 Day 7 - Bridge Repair """
+
+from paoc.helper import get_input, print_summary
+import re
+from itertools import product
+
+
+def total_calibration(lines: list[str], op_chars: str = '+*') -> int:
+    total = 0
+    for line in lines:
+        val, *nums = map(int, re.findall(r'\d+', line))
+        for ops in product(op_chars, repeat=len(nums)-1):
+            x = nums[0]
+            for op, num in zip(ops, nums[1:]):
+                if op == '+':   x += num
+                elif op == '*': x *= num
+                else: x = int(str(x) + str(num))
+            if x == val:
+                total += val
+                break
+    return total
+
+def p1() -> any:
+    return total_calibration(get_input(7))
+
+def p2() -> any:
+    return total_calibration(get_input(7), op_chars='+*|')
+
+
+if __name__ == '__main__':
+    print_summary(__file__, p1, p2, n=1)
