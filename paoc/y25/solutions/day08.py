fae7b6a803c2aa1c87ae8a49718db9985c0b8cff
Josef Hamelink <91252200+Josef-Hlink@users.noreply.github.com>
2025-12-09 00:59:43 +0100

    solve d8 (dirty as hell)
    not DRY in the slightest

--- paoc/y25/solutions/day08.py
+++ paoc/y25/solutions/day08.py
@@ -15,48 +15,57 @@ def dist(b1: tuple[int, ...], b2: tuple[int, ...]) -> float:
 
 def p1() -> int:
     lines = get_input(8)
-    # lines = get_input(8, True)
     boxes = [tuple(map(int, re.findall(r'\d+', l))) for l in lines]
-    print(len(boxes))
     dists = [(b1, b2, dist(b1, b2)) for i, b1 in enumerate(boxes) for b2 in boxes[i + 1 :]]
     dists.sort(key=lambda t: t[2])
     circuits: list[set[tuple[int, ...]]] = []
     for _ in range(1000):
         b1, b2, _ = dists.pop(0)
+        connections = []
         for i, circuit in enumerate(circuits):
             if b1 in circuit or b2 in circuit:
-                circuits[i] = circuit | {b1, b2}
-                break
+                connections.append(i)
+        if len(connections) == 1:
+            circuits[connections[0]] |= {b1, b2}
+        elif len(connections) > 1:
+            new_circuit = set()
+            for i in sorted(connections, reverse=True):
+                new_circuit |= circuits.pop(i)
+            new_circuit |= {b1, b2}
+            circuits.append(new_circuit)
         else:
             circuits.append({b1, b2})
-        # check if we can do any merges
-        final_circuits = []
-        for i, circuit in enumerate(circuits):
-            coll = circuit
-            for j, oth in enumerate(circuits[i + 1 :]):
-                if circuit.intersection(oth):
-                    coll |= oth
-                    circuits[j] = set()
-            final_circuits.append(coll)
-        circuits = final_circuits
-
-    for fc in sorted(map(len, circuits), reverse=True):
-        if fc < 3:
-            break
-        print(fc)
-
     return math.prod(list(sorted(map(len, circuits), reverse=True))[:3])
 
 
 def p2() -> int:
     lines = get_input(8)
-    lines = get_input(8, True)
-    _ = lines
-    return 0
+    boxes = [tuple(map(int, re.findall(r'\d+', l))) for l in lines]
+    lb = len(boxes)
+    dists = [(b1, b2, dist(b1, b2)) for i, b1 in enumerate(boxes) for b2 in boxes[i + 1 :]]
+    dists.sort(key=lambda t: t[2])
+    circuits: list[set[tuple[int, ...]]] = []
+    b1x, b2x = 0, 0
+    for b1, b2, _ in dists:
+        connections = []
+        for i, circuit in enumerate(circuits):
+            if b1 in circuit or b2 in circuit:
+                connections.append(i)
+        if len(connections) == 1:
+            circuits[connections[0]] |= {b1, b2}
+        elif len(connections) > 1:
+            new_circuit = set()
+            for i in sorted(connections, reverse=True):
+                new_circuit |= circuits.pop(i)
+            new_circuit |= {b1, b2}
+            circuits.append(new_circuit)
+        else:
+            circuits.append({b1, b2})
+        if len(circuits) == 1 and len(circuits[0]) == lb:
+            b1x, b2x = b1[0], b2[0]
+            break
+    return b1x * b2x
 
 
 if __name__ == '__main__':
-    print(p1())
-    print(p2())
-    exit()
-    print_summary(__file__, p1, p2)
+    print_summary(__file__, p1, p2, n=10)
