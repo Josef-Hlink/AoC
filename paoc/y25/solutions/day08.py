e7bd077fadb007d07f5aa41d40f84bc9b7912f8d
Josef Hamelink <91252200+Josef-Hlink@users.noreply.github.com>
2025-12-09 01:19:40 +0100

    refactor[8]: clean up
    i can live with this

--- paoc/y25/solutions/day08.py
+++ paoc/y25/solutions/day08.py
@@ -3,8 +3,8 @@
 
 """AoC 25 Day 8 - Playground"""
 
-import re
 import math
+import re
 
 from paoc.helper import get_input, print_summary
 
@@ -13,55 +13,54 @@ def dist(b1: tuple[int, ...], b2: tuple[int, ...]) -> float:
     return math.sqrt((b1[0] - b2[0]) ** 2 + (b1[1] - b2[1]) ** 2 + (b1[2] - b2[2]) ** 2)
 
 
+def update_circuits(
+    circuits: list[set[tuple[int, ...]]],
+    b1: tuple[int, ...],
+    b2: tuple[int, ...],
+) -> None:
+    """Update circuits with new box connection. Modifies circuits list in place!"""
+
+    # Find indices of circuits that connect to b1 or b2
+    connections: list[int] = []
+    for i, circuit in enumerate(circuits):
+        if b1 in circuit or b2 in circuit:
+            connections.append(i)
+
+    # Update circuits based on connections found
+    if len(connections) == 1:
+        circuits[connections[0]] |= {b1, b2}
+    elif len(connections) == 2:
+        new_circuit = set()
+        for i in sorted(connections, reverse=True):  # reverse to avoid index shift
+            new_circuit |= circuits.pop(i)
+        new_circuit |= {b1, b2}
+        circuits.append(new_circuit)
+    else:
+        circuits.append({b1, b2})
+
+
 def p1() -> int:
-    lines = get_input(8)
-    boxes = [tuple(map(int, re.findall(r'\d+', l))) for l in lines]
+    boxes = [tuple(map(int, re.findall(r'\d+', l))) for l in get_input(8)]
     dists = [(b1, b2, dist(b1, b2)) for i, b1 in enumerate(boxes) for b2 in boxes[i + 1 :]]
-    dists.sort(key=lambda t: t[2])
     circuits: list[set[tuple[int, ...]]] = []
-    for _ in range(1000):
-        b1, b2, _ = dists.pop(0)
-        connections = []
-        for i, circuit in enumerate(circuits):
-            if b1 in circuit or b2 in circuit:
-                connections.append(i)
-        if len(connections) == 1:
-            circuits[connections[0]] |= {b1, b2}
-        elif len(connections) > 1:
-            new_circuit = set()
-            for i in sorted(connections, reverse=True):
-                new_circuit |= circuits.pop(i)
-            new_circuit |= {b1, b2}
-            circuits.append(new_circuit)
-        else:
-            circuits.append({b1, b2})
+
+    # Make 1000 closest connections and return product of sizes of 3 largest circuits
+    for b1, b2, _ in sorted(dists, key=lambda tup: tup[2])[:1000]:
+        update_circuits(circuits, b1, b2)
     return math.prod(list(sorted(map(len, circuits), reverse=True))[:3])
 
 
 def p2() -> int:
-    lines = get_input(8)
-    boxes = [tuple(map(int, re.findall(r'\d+', l))) for l in lines]
-    lb = len(boxes)
+    boxes = [tuple(map(int, re.findall(r'\d+', l))) for l in get_input(8)]
     dists = [(b1, b2, dist(b1, b2)) for i, b1 in enumerate(boxes) for b2 in boxes[i + 1 :]]
-    dists.sort(key=lambda t: t[2])
     circuits: list[set[tuple[int, ...]]] = []
+
+    # Connect boxes until all are in one circuit and return
+    # product of the x coordinates of the last connected boxes
     b1x, b2x = 0, 0
-    for b1, b2, _ in dists:
-        connections = []
-        for i, circuit in enumerate(circuits):
-            if b1 in circuit or b2 in circuit:
-                connections.append(i)
-        if len(connections) == 1:
-            circuits[connections[0]] |= {b1, b2}
-        elif len(connections) > 1:
-            new_circuit = set()
-            for i in sorted(connections, reverse=True):
-                new_circuit |= circuits.pop(i)
-            new_circuit |= {b1, b2}
-            circuits.append(new_circuit)
-        else:
-            circuits.append({b1, b2})
-        if len(circuits) == 1 and len(circuits[0]) == lb:
+    for b1, b2, _ in sorted(dists, key=lambda tup: tup[2]):
+        update_circuits(circuits, b1, b2)
+        if len(circuits) == 1 and len(circuits[0]) == len(boxes):
             b1x, b2x = b1[0], b2[0]
             break
     return b1x * b2x
